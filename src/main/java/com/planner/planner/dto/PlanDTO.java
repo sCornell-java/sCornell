b5472007f0042fa4ff8a812f97ace6c0649b054b
choiyh0617 <6l0mrc00057@gmail.com>
2025-06-24 11:40:21 +0900

    run

--- src/main/java/com/planner/planner/dto/PlanDTO.java
+++ src/main/java/com/planner/planner/dto/PlanDTO.java
@@ -4,14 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.time.LocalDate;
 
 @Setter
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ItemDTO {
+public class PlanDTO {
     private String id;
     private String title;
     private String detail;
-    private String date;
+    private LocalDate date;
+    private boolean success;
 }
