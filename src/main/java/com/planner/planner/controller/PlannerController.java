2de2d7c363752dc0e5fcac5be703dc722a175005
choiyh0617 <6l0mrc00057@gmail.com>
2025-06-24 11:01:49 +0900

    delete

--- src/main/java/com/planner/planner/controller/PlannerController.java
+++ src/main/java/com/planner/planner/controller/PlannerController.java
@@ -1,8 +1,11 @@
 package com.planner.planner.controller;
 
+import com.planner.planner.dto.ResponseDTO;
 import com.planner.planner.service.PlannerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PlannerController {
     @Autowired
     private PlannerService plannerService;
+
+    @DeleteMapping("goals/delete")
+    public ResponseDTO deletePlan(@RequestParam("id") String id) {
+        log.info("deletePlan:{}", id);
+        boolean delete = plannerService.deletePlan(id);
+        return new ResponseDTO(delete ? "삭제되었습니다." : "없는 계획이거나 삭제된 계획입니다.");
+    }
 }
--- src/main/java/com/planner/planner/service/PlannerService.java
+++ src/main/java/com/planner/planner/service/PlannerService.java
@@ -1,9 +1,23 @@
 package com.planner.planner.service;
 
+import com.planner.planner.repository.PlanRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class PlannerService {
+    private final PlanRepository planRepository;
+
+    public PlannerService(PlanRepository planRepository) {
+        this.planRepository = planRepository;
+    }
+
+    public boolean deletePlan(String id) {
+        if (planRepository.existsById(id)) {
+            planRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
