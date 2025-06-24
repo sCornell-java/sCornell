d6477e5e1710ead1213af303a4103cbba8e871d2
choiyh0617 <6l0mrc00057@gmail.com>
2025-06-24 11:31:52 +0900

    deleteplan

--- src/main/java/com/planner/planner/service/PlannerService.java
+++ src/main/java/com/planner/planner/service/PlannerService.java
@@ -2,22 +2,23 @@ package com.planner.planner.service;
 
 import com.planner.planner.repository.PlanRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
 public class PlannerService {
-    private final PlanRepository planRepository;
-
-    public PlannerService(PlanRepository planRepository) {
-        this.planRepository = planRepository;
-    }
+    @Autowired
+    PlanRepository planRepository;
 
     public boolean deletePlan(String id) {
         if (planRepository.existsById(id)) {
             planRepository.deleteById(id);
+            System.out.println("Plan has been deleted");
             return true;
         }
+        System.out.println("Plan could not be deleted");
+        System.out.println(id);
         return false;
     }
 }
